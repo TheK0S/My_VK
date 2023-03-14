1b45211f1eae3d9dd3077ee645b7f80b3cc7f7c9
Konstantin <turchakkonstantin@gmail.com>
2023-03-14 16:46:11 +0200

    registration

--- My_VK/Form1.Designer.cs
+++ My_VK/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formAuthorization));
             this.iconVK = new System.Windows.Forms.PictureBox();
             this.labelEnterEmail = new System.Windows.Forms.Label();
@@ -37,14 +38,18 @@
             this.passwordField = new System.Windows.Forms.TextBox();
             this.checkBoxSaveEnterData = new System.Windows.Forms.CheckBox();
             this.Confirm = new System.Windows.Forms.Button();
+            this.popUpWindow = new System.Windows.Forms.ToolTip(this.components);
+            this.registrationButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.iconVK)).BeginInit();
             this.SuspendLayout();
             // 
             // iconVK
             // 
+            this.iconVK.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.iconVK.Image = ((System.Drawing.Image)(resources.GetObject("iconVK.Image")));
             this.iconVK.InitialImage = ((System.Drawing.Image)(resources.GetObject("iconVK.InitialImage")));
-            this.iconVK.Location = new System.Drawing.Point(395, 32);
+            this.iconVK.Location = new System.Drawing.Point(396, 31);
             this.iconVK.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.iconVK.Name = "iconVK";
             this.iconVK.Size = new System.Drawing.Size(87, 99);
@@ -54,26 +59,31 @@
             // 
             // labelEnterEmail
             // 
+            this.labelEnterEmail.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.labelEnterEmail.AutoSize = true;
             this.labelEnterEmail.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.labelEnterEmail.Location = new System.Drawing.Point(365, 143);
+            this.labelEnterEmail.Location = new System.Drawing.Point(367, 142);
             this.labelEnterEmail.Name = "labelEnterEmail";
             this.labelEnterEmail.Size = new System.Drawing.Size(145, 22);
             this.labelEnterEmail.TabIndex = 1;
             this.labelEnterEmail.Text = "Введите номер";
+            this.labelEnterEmail.Click += new System.EventHandler(this.labelEnterEmail_Click);
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label1.Location = new System.Drawing.Point(279, 177);
+            this.label1.Location = new System.Drawing.Point(285, 173);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(308, 44);
             this.label1.TabIndex = 2;
             this.label1.Text = "Ваш номер телефона будет использоватся\r\n                    для входа в аакаунт";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // phoneNumberField
             // 
-            this.phoneNumberField.Location = new System.Drawing.Point(319, 228);
+            this.phoneNumberField.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.phoneNumberField.Location = new System.Drawing.Point(321, 229);
             this.phoneNumberField.Name = "phoneNumberField";
             this.phoneNumberField.Size = new System.Drawing.Size(237, 27);
             this.phoneNumberField.TabIndex = 3;
@@ -81,9 +91,10 @@
             // 
             // labelEnterPassword
             // 
+            this.labelEnterPassword.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.labelEnterPassword.AutoSize = true;
             this.labelEnterPassword.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.labelEnterPassword.Location = new System.Drawing.Point(364, 277);
+            this.labelEnterPassword.Location = new System.Drawing.Point(364, 272);
             this.labelEnterPassword.Name = "labelEnterPassword";
             this.labelEnterPassword.Size = new System.Drawing.Size(151, 22);
             this.labelEnterPassword.TabIndex = 4;
@@ -91,7 +102,8 @@
             // 
             // passwordField
             // 
-            this.passwordField.Location = new System.Drawing.Point(319, 318);
+            this.passwordField.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.passwordField.Location = new System.Drawing.Point(321, 309);
             this.passwordField.Name = "passwordField";
             this.passwordField.PasswordChar = '*';
             this.passwordField.Size = new System.Drawing.Size(237, 27);
@@ -99,37 +111,77 @@
             // 
             // checkBoxSaveEnterData
             // 
+            this.checkBoxSaveEnterData.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.checkBoxSaveEnterData.AutoSize = true;
             this.checkBoxSaveEnterData.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.checkBoxSaveEnterData.Location = new System.Drawing.Point(321, 360);
+            this.checkBoxSaveEnterData.Location = new System.Drawing.Point(370, 350);
             this.checkBoxSaveEnterData.Name = "checkBoxSaveEnterData";
             this.checkBoxSaveEnterData.Size = new System.Drawing.Size(138, 24);
             this.checkBoxSaveEnterData.TabIndex = 6;
             this.checkBoxSaveEnterData.Text = "Сохранить вход";
             this.checkBoxSaveEnterData.UseVisualStyleBackColor = true;
+            this.checkBoxSaveEnterData.MouseLeave += new System.EventHandler(this.checkBoxSaveEnterData_MouseLeave);
+            this.checkBoxSaveEnterData.MouseHover += new System.EventHandler(this.checkBoxSaveEnterData_MouseHover);
             // 
             // Confirm
             // 
+            this.Confirm.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.Confirm.BackColor = System.Drawing.Color.CornflowerBlue;
             this.Confirm.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Confirm.FlatAppearance.BorderColor = System.Drawing.Color.Red;
             this.Confirm.FlatAppearance.BorderSize = 0;
-            this.Confirm.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Confirm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Confirm.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.Confirm.ForeColor = System.Drawing.SystemColors.Window;
-            this.Confirm.Location = new System.Drawing.Point(321, 402);
+            this.Confirm.Location = new System.Drawing.Point(321, 389);
             this.Confirm.Name = "Confirm";
-            this.Confirm.Size = new System.Drawing.Size(237, 27);
+            this.Confirm.Size = new System.Drawing.Size(237, 30);
             this.Confirm.TabIndex = 7;
             this.Confirm.Text = "Продолжить";
             this.Confirm.UseVisualStyleBackColor = false;
+            // 
+            // popUpWindow
+            // 
+            this.popUpWindow.BackColor = System.Drawing.SystemColors.Menu;
+            this.popUpWindow.ToolTipTitle = "Сохранить вход";
+            // 
+            // registrationButton
+            // 
+            this.registrationButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.registrationButton.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.registrationButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.registrationButton.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.registrationButton.FlatAppearance.BorderSize = 0;
+            this.registrationButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.registrationButton.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.registrationButton.ForeColor = System.Drawing.SystemColors.Window;
+            this.registrationButton.Location = new System.Drawing.Point(321, 469);
+            this.registrationButton.Name = "registrationButton";
+            this.registrationButton.Size = new System.Drawing.Size(237, 30);
+            this.registrationButton.TabIndex = 8;
+            this.registrationButton.Text = "Зарегистрироваться";
+            this.registrationButton.UseVisualStyleBackColor = false;
+            this.registrationButton.Click += new System.EventHandler(this.registrationButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(321, 428);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(237, 30);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Если у вас нет учетной записи";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // formAuthorization
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(914, 600);
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.registrationButton);
             this.Controls.Add(this.Confirm);
             this.Controls.Add(this.checkBoxSaveEnterData);
             this.Controls.Add(this.passwordField);
@@ -141,7 +193,8 @@
             this.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "formAuthorization";
-            this.Text = "Form1";
+            this.Text = "My VK";
+            this.Load += new System.EventHandler(this.formAuthorization_Load);
             ((System.ComponentModel.ISupportInitialize)(this.iconVK)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -158,5 +211,8 @@
         private TextBox passwordField;
         private CheckBox checkBoxSaveEnterData;
         private Button Confirm;
+        private ToolTip popUpWindow;
+        private Button registrationButton;
+        private Label label2;
     }
 }
--- My_VK/Form1.cs
+++ My_VK/Form1.cs
@@ -4,17 +4,18 @@ namespace My_VK
 {
     public partial class formAuthorization : Form
     {
-        string cond = @"(\w + ([-+.]\w +)*@\w + ([-.]\w +)*\.\w + ([-.]\w +)*)";
-        string condNum = @"[+,1 - 9,0]";
+        string condMail = @"^[a-zA-Z\.\-_]+@([a-zA-Z\.\-_]+\.)+[a-zA-Z]{2,4}$";
+        string condNum = @"^\+\d{12}$";
         
         public formAuthorization()
         {
             InitializeComponent();
+            popUpWindow.SetToolTip(checkBoxSaveEnterData, "Выберите, чтобы сохранить\nданные аккаунта для быстрого\nвхода на этом устройстве");
         }
 
         private void phoneNumberField_TextChanged(object sender, EventArgs e)
         {
-            if(Regex.IsMatch(phoneNumberField.Text, cond) || (Regex.IsMatch(phoneNumberField.Text, condNum) && phoneNumberField.TextLength > 9))
+            if(Regex.IsMatch(phoneNumberField.Text, condMail) || Regex.IsMatch(phoneNumberField.Text, condNum))
             {
                 Confirm.Enabled = true;
                 phoneNumberField.ForeColor = Color.Black;
@@ -24,6 +25,38 @@ namespace My_VK
                 Confirm.Enabled = false;
                 phoneNumberField.ForeColor = Color.Red;
             }
+        }
+
+        private void checkBoxSaveEnterData_MouseHover(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkBoxSaveEnterData_MouseLeave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void formAuthorization_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelEnterEmail_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void registrationButton_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            var formRegistaration = new FormRegistration();
+            formRegistaration.Show();
         }
     }
 }
